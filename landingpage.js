30c62eb7ec90a419ceecbf8c67cdf3af0fe31671
unknown <vaibhavdewan3@gmail.com>
2024-11-10 13:24:48 +0530

    Initial commit

---
+++ landingpage.js
@@ -0,0 +1,7 @@
+document.getElementById("userPanelBtn").addEventListener("click", function() {
+    window.location.href ="index.html"; // Redirect to user panel
+});
+
+document.getElementById("adminPanelBtn").addEventListener("click", function() {
+    window.location.href ="admin.html"; // Redirect to admin panel
+});
