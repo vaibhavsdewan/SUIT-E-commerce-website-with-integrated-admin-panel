74424b0d1a3a6aa12e55ee0b2661799b62c3877f
unknown <vaibhavdewan3@gmail.com>
2024-11-17 01:44:24 +0530

    Updated project files with new features and bug fixes

--- landingpage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("userPanelBtn").addEventListener("click", function() {
-    window.location.href ="index.html"; // Redirect to user panel
-});
-
-document.getElementById("adminPanelBtn").addEventListener("click", function() {
-    window.location.href ="admin.html"; // Redirect to admin panel
-});
